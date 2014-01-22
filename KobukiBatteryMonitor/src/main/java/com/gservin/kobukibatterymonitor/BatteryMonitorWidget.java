51c1b87bb68edda9181554b5f96cdea2c5e27193
Gary Servin <garyservin@gmail.com>
2014-01-22 19:30:55 -0300

    Testing Widget Update

--- KobukiBatteryMonitor/src/main/java/com/gservin/kobukibatterymonitor/BatteryMonitorWidget.java
+++ KobukiBatteryMonitor/src/main/java/com/gservin/kobukibatterymonitor/BatteryMonitorWidget.java
@@ -2,7 +2,6 @@ package com.gservin.kobukibatterymonitor;
 
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
-import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
@@ -11,36 +10,96 @@ import android.os.BatteryManager;
 import android.widget.RemoteViews;
 
 public class BatteryMonitorWidget extends AppWidgetProvider {
+    int batteryLevel = 0;
+    String phoneCharging = "";
+
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         super.onUpdate(context, appWidgetManager, appWidgetIds);
-
-        Intent batteryIntent = context.getApplicationContext().registerReceiver(mBatInfoReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
     }
 
-    private BroadcastReceiver mBatInfoReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            String phoneCharging = "";
-            int batteryLevel = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context.getApplicationContext());
+        ComponentName thisWidget = new ComponentName(context.getApplicationContext(), BatteryMonitorWidget.class);
+
+        int[] appWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
+        if (appWidgetIds != null && appWidgetIds.length > 0) {
+//            onUpdate(context, appWidgetManager, appWidgetIds);
+
+            IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+            Intent batteryStatus = context.registerReceiver(null, ifilter);
+
+            batteryLevel = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
 
             // Are we charging / charged?
-            int status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+            int status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
             boolean phoneIsCharging = status == BatteryManager.BATTERY_STATUS_CHARGING ||
                     status == BatteryManager.BATTERY_STATUS_FULL;
 
-            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
-            views.setProgressBar(R.id.phone_progressBar, 100, batteryLevel, false);
-
             if (phoneIsCharging){
                 phoneCharging = "\t|\tCharging";
+            }else{
+                phoneCharging = "";
             }
-            views.setTextViewText(R.id.phone_charging_text, batteryLevel + "%" + phoneCharging);
-
-            ComponentName componentName = new ComponentName(context, BatteryMonitorWidget.class);
-            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
-            appWidgetManager.updateAppWidget(componentName, views);
+            updateViews(context);
         }
-    };
+    }
+
+    private void calculateBatteryLevel(Context context) {
+        IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent batteryStatus = context.registerReceiver(null, ifilter);
+
+        batteryLevel = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+
+        // Are we charging / charged?
+        int status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+        boolean phoneIsCharging = status == BatteryManager.BATTERY_STATUS_CHARGING ||
+                status == BatteryManager.BATTERY_STATUS_FULL;
+
+        if (phoneIsCharging){
+            phoneCharging = "\t|\tCharging";
+        }else{
+            phoneCharging = "";
+        }
+    }
+
+    private void updateViews(Context context) {
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+
+        views.setTextViewText(R.id.phone_charging_text, batteryLevel + "%" + phoneCharging);
+        views.setProgressBar(R.id.phone_progressBar, 100, batteryLevel, false);
+
+        ComponentName componentName = new ComponentName(context, BatteryMonitorWidget.class);
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+        appWidgetManager.updateAppWidget(componentName, views);
+    }
+
+//    private BroadcastReceiver mBatInfoReceiver = new BroadcastReceiver() {
+//        @Override
+//        public void onReceive(Context context, Intent intent) {
+//            String phoneCharging = "";
+//            int batteryLevel = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+//
+//            // Are we charging / charged?
+//            int status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+//            boolean phoneIsCharging = status == BatteryManager.BATTERY_STATUS_CHARGING ||
+//                    status == BatteryManager.BATTERY_STATUS_FULL;
+//
+//            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+//            views.setProgressBar(R.id.phone_progressBar, 100, batteryLevel, false);
+//
+//            if (phoneIsCharging){
+//                phoneCharging = "\t|\tCharging";
+//            }
+//            views.setTextViewText(R.id.phone_charging_text, batteryLevel + "%" + phoneCharging);
+//
+//            ComponentName componentName = new ComponentName(context, BatteryMonitorWidget.class);
+//
+//            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+//            appWidgetManager.updateAppWidget(componentName, views);
+//        }
+//    };
 }
